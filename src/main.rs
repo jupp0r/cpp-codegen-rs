60b0fe29a5199022aff8c0ed3cc7439a058ee435
Jupp Müller <jupp0r@gmail.com>
2016-07-17 15:11:26 +0200

    Parse simple interface

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,6 @@ fn main() {
     let clang = Clang::new().unwrap();
     let index = Index::new(&clang, false, false);
     let tu = index.parser("interface.h").arguments(&[&"-x", &"c++"]).parse().unwrap();
-    let _ = tu.get_entity().visit_children(visitor::visit);
+    let model = visitor::Model::new(&tu);
+    println!("{:?}", model);
 }
---
+++ src/visitor.rs
@@ -0,0 +1,111 @@
+use clang::{Entity, Type, EntityKind, EntityVisitResult, TranslationUnit};
+use std::vec::Vec;
+use std::string::String;
+use std::option::Option;
+
+#[derive(Debug)]
+pub struct Model {
+    interfaces: Vec<Interface>,
+}
+
+#[derive(Debug)]
+pub struct Interface {
+    name: String,
+    namespaces: Vec<String>,
+    methods: Vec<Method>,
+}
+
+#[derive(Debug)]
+pub struct Method {
+    name: String,
+    return_type: String,
+    arguments: Vec<Argument>,
+}
+
+#[derive(Debug)]
+pub struct Argument {
+    name: Option<String>,
+    argument_type: String,
+}
+
+impl Model {
+    pub fn new(tu: &TranslationUnit) -> Model {
+        let mut model = Model { interfaces: vec![] };
+        {
+            let visitor = |node: Entity, parent: Entity| build_model(&mut model, node, parent);
+            tu.get_entity().visit_children(visitor);
+        }
+        model
+    }
+}
+
+
+
+fn build_model(model: &mut Model, node: Entity, parent: Entity) -> EntityVisitResult {
+    if !node.is_in_main_file() {
+        return EntityVisitResult::Continue;
+    }
+
+    if !is_interface(node) {
+        return EntityVisitResult::Continue;
+    }
+
+    if node.get_kind() == EntityKind::StructDecl {
+        model.interfaces.push(Interface {
+            name: node.get_name().unwrap_or("unknown".to_string()),
+            namespaces: parse_namespaces(parent),
+            methods: parse_pure_virtual_methods(node),
+        });
+        EntityVisitResult::Continue
+    } else {
+        EntityVisitResult::Recurse
+    }
+}
+
+fn parse_namespaces(node: Entity) -> Vec<String> {
+    let mut pointer = node;
+    let mut namespace_list = vec![];
+    while pointer.get_kind() == EntityKind::Namespace {
+        namespace_list.push(pointer.get_name().unwrap());
+        match pointer.get_lexical_parent() {
+            Some(n) => pointer = n,
+            None => break,
+        }
+    }
+    namespace_list.reverse();
+    namespace_list
+}
+
+fn parse_pure_virtual_methods(node: Entity) -> Vec<Method> {
+    node.get_children()
+        .into_iter()
+        .filter(|method| method.is_pure_virtual_method())
+        .map(parse_method)
+        .collect::<Vec<_>>()
+}
+
+fn parse_method(node: Entity) -> Method {
+    Method {
+        name: node.get_name().unwrap(),
+        return_type: "void".to_string(),
+        arguments: parse_arguments(node),
+    }
+}
+
+fn parse_arguments(node: Entity) -> Vec<Argument> {
+    node.get_arguments().map_or(vec![],
+                                |t| t.into_iter().map(parse_argument).collect::<Vec<_>>())
+}
+
+fn parse_argument(t: Entity) -> Argument {
+    Argument {
+        name: t.get_name(),
+        argument_type: t.get_type().unwrap().get_display_name(),
+    }
+}
+
+fn is_interface(node: Entity) -> bool {
+    node.get_children().into_iter().any(|method| {
+        method.get_kind() == EntityKind::Destructor || !method.is_pure_virtual_method()
+    })
+}
