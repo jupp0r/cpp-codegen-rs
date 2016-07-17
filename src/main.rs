d2a69840d9c42a4c68c9c134186e9605a3c704b8
Jupp Müller <jupp0r@gmail.com>
2016-07-17 03:02:48 +0200

    Build simple example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate clang;
+
+use clang::{Clang, Index};
+
+mod visitor;
+
 fn main() {
-    println!("Hello, world!");
+    let clang = Clang::new().unwrap();
+    let index = Index::new(&clang, false, false);
+    let tu = index.parser("interface.h").arguments(&[&"-x", &"c++"]).parse().unwrap();
+    let _ = tu.get_entity().visit_children(visitor::visit);
 }
